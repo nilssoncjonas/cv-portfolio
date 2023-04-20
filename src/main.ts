e461b83a8ef53ce9666c31cab2abc1c04f77c582
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-04-20 21:56:03 +0200

    started on header, nav, menu

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './scss/style.scss'
+
+// DOM querySelector
+const menuToggle = document.querySelector('#menuToggle') as HTMLSpanElement
+const menuWrap = document.querySelector('#menuWrap') as HTMLDivElement
+
+menuToggle.addEventListener('click', () => {
+	menuWrap.classList.toggle('show')
+})
