ba4dcd319cf77627a76be87fb6901035d241c012
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-04-21 19:27:03 +0200

    can't scroll when menu is open

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import './scss/style.scss'
 // DOM querySelector
 const menuToggle = document.querySelector('#menuToggle') as HTMLSpanElement
 const menuWrap = document.querySelector('#menuWrap') as HTMLDivElement
+const body = document.querySelector('#body') as HTMLBodyElement
 
+// menuToggle
 menuToggle.addEventListener('click', () => {
 	menuWrap.classList.toggle('show')
+	body.classList.toggle('fixed')
 })
