ca45d1d3c23c0d176d1655502232db30a14c62ff
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-04-24 10:51:23 +0200

    added a back to top arrow, show when scroll-up

--- src/main.ts
+++ src/main.ts
@@ -7,32 +7,49 @@ const menu = document.querySelector('.menu') as HTMLUListElement
 const menuItems = Array.from(document.querySelectorAll('.menuItem')) as HTMLLIElement[]
 const body = document.querySelector('#body') as HTMLBodyElement
 
-// const date = new Date();
-// const year = date.getFullYear();
-// const month = date.getMonth() + 1;
-// const day = date.getDate();
-// const hours = date.getHours();
-// const minutes = date.getMinutes();
-// const seconds = date.getSeconds();
+const bttopEl = document.querySelector('#test') as HTMLSpanElement
 
-// const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
 const timeStamp = () => new Date().toLocaleString("sv-SE", { timeZone: "Europe/Stockholm" });
 
 
 // functions
 const toggleMenu = () => {
-	console.log('togglemenu @',timeStamp())
+	console.log('togglemenu @', timeStamp())
 	menu.classList.toggle('show')
 	menuWrap.classList.toggle('show')
 	body.classList.toggle('fixed')
 }
 
+
+
 // evenlisteners
-menuToggle.addEventListener('click',toggleMenu) 
+menuToggle.addEventListener('click', toggleMenu)
 
 menuItems.forEach(
-	function(menuItem) {
+	function (menuItem) {
 		menuItem.addEventListener('click', toggleMenu)
 	}
 )
+
+
+
+
+let scrollPos = 0;
+
+
+const checkPosition = () => {
+  let windowY = window.scrollY;
+  if (windowY < scrollPos) {
+    // Scrolling UP
+    bttopEl.classList.add('bttop__show');
+    bttopEl.classList.remove('bttop__hide');
+  } else {
+    // Scrolling DOWN
+    bttopEl.classList.add('bttop__hide');
+    bttopEl.classList.remove('bttop__show');
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener('scroll', checkPosition);
