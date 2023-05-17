8f989ec7379c007dc1ac0ec60a558d5c6bc14d5f
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-05-17 11:50:29 +0200

    added hosted BASE_URL to api fetch

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 
 
-
-const BASE_URL ='http://localhost:3000'
+// local development
+// const BASE_URL ='http://localhost:3000'
+// hosted development
+const BASE_URL ='https://thoughtful-beanie-wasp.cyclic.app'
 
 export const fetchProject = async () => {
 	const res = await fetch(`${BASE_URL}/project`)
